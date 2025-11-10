907a218bb46d8283f3f156be8fa1f6c65fed08c2
Erfan Yousefi <75472834+erfanyousefi@users.noreply.github.com>
2025-11-10 16:00:39 +0330

    add swagger url in logs

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   SwaggerConfigInit(app)
   await app.listen(3000, () => {
     console.log("http://localhost:3000")
+    console.log("http://localhost:3000/swagger")
+    
   });
 }
 bootstrap();
